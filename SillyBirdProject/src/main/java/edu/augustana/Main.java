f01166332d3dbac75164ace133cc1d94314eaaf6
NhatMinhPhan <118093443+Phannhatminh@users.noreply.github.com>
2024-09-26 09:53:17 -0500

    fix a bug

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -3,8 +3,8 @@ package edu.augustana;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
+        Chicken chicken = new Chicken();
+        chicken.dance();
     }
-    Chicken chicken = new Chicken();
-    chicken.dance();
 
 }
